b72b514bd8380cee87ec6222c8b02b016870b518
Kyle Mullins <kylebmullins@gmail.com>
2021-03-25 11:46:04 -0600

    Push test

--- NewRemind.java
+++ NewRemind.java
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import java.io.*;
 public class NewRemind {
     public static void newRemind() {
         
     }
-=======
-import java.io.*;
-public class NewRemind {
-    public static void newRemind() {
-        
-    }
->>>>>>> upstream/main
 }
