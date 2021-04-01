24b656de6e35a4933117b8f367e6d2b874c7a348
Sophia Tsai <tsaisk@s.dcsdk12.org>
2021-04-01 11:52:29 -0600

    Update

--- NewRemind.java
+++ NewRemind.java
@@ -1,12 +1,24 @@
 import java.io.*;
+import java.util.Scanner;
 public class NewRemind {
-    public NewRemind(String remindName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
-        
+    Scanner scan = new Scanner(System.in);
+    String fileName = "", event = "";
+    int day, month, year, time;
 
 
-    }
+public NewRemind(String remindName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
+    makeEvent(remindName, "'" + reminder + "'", day, month, year);
+}
 
-    public NewRemind(String remindName, String reminder, int day, int month, int year, String time) throws IOException, FileNotFoundException {
-        
+public void makeEvent(String fileName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
+    File file = new File (fileName + ".md");
+
+    String write = reminder + " " + day + "/" + month + "/" + year;
+
+    PrintWriter game = new PrintWriter(fileName + ".md");
+
+    game.write(write);
+    game.flush();
+    game.close();
     }
 }
---
+++ Tester.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Tester {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
+        getRemind test = new getRemind("test", "This is a test", 1, 1, 22);
+        
+    }
+}
