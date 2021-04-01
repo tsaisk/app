90bc5388610a98f082ff41597d25451894d6e187
Sophia Tsai <tsaisk@s.dcsdk12.org>
2021-04-01 11:59:29 -0600

    stores event

--- NewRemind.java
+++ NewRemind.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class NewRemind {
     Scanner scan = new Scanner(System.in);
     String fileName = "", event = "";
-    int day, month, year, time;
+    int day, month, year, hour, minute;
 
 
 public NewRemind(String remindName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
     makeEvent(remindName, "'" + reminder + "'", day, month, year);
 }
+    public NewRemind(String remindName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
+        makeEvent(remindName, "'" + reminder + "'", day, month, year, hour, minute);
+    }
 
 public void makeEvent(String fileName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
     File file = new File (fileName + ".md");
@@ -21,4 +24,16 @@ public void makeEvent(String fileName, String reminder, int day, int month, int 
     game.flush();
     game.close();
     }
+
+    public void makeEvent(String fileName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
+        File file = new File (fileName + ".md");
+    
+        String write = reminder + " " + day + "/" + month + "/" + year + " " + hour + ":" + minute;
+    
+        PrintWriter game = new PrintWriter(fileName + ".md");
+    
+        game.write(write);
+        game.flush();
+        game.close();
+        }
 }
