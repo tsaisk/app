60f36e6eb1a509d6533965714f5dce4555c768af
Chandan Marle <chandanmarlex12@gmail.com>
2021-04-07 10:55:23 -0600

    Create UI

--- Interface.java
+++ Interface.java
@@ -2,16 +2,67 @@ import javax.swing.*;
 public class Interface {
     
     public static void main(String[] args){
+        createFrameHome();
+
+        
+    }
+
+
+    static void createFrameHome(){
         JFrame frame=new JFrame();  
         frame.setSize(1000,1000);
-        JButton a=new JButton("View Current Events");//creating instance of JButton  
-        a.setBounds(300,0,300, 100);  
-        JButton b=new JButton("Create New Event");//creating instance of JButton  
-        b.setBounds(0,0,300, 100);  
-        frame.add(a);
-        frame.add(b);
+
+        JButton currentEvents=new JButton("View Current Events");
+        currentEvents.setBounds(350,100,300, 100);  
+        currentEvents.addActionListener((e)-> {
+            frame.dispose();
+            createFrameNewEvents();
+        });
+
+        JButton newEvents=new JButton("Create New Event");
+        newEvents.setBounds(0,100,300, 100);
+        newEvents.addActionListener((e)-> {
+            frame.dispose();
+            createFrameNewEvents();
+        });
+
+        JButton edit=new JButton("Edit");
+        edit.setBounds(700,100,300, 100);
+        edit.addActionListener((e)-> {
+            frame.dispose();
+            createFrameEdit();
+        });
+
+        JLabel welcome;
+        welcome = new JLabel("Welcome to App");
+        // welcome.setBounds(500,0, 100,300);
+
+        frame.add(newEvents);
+        frame.add(currentEvents);
+        frame.add(edit);        
+        frame.add(welcome);
         frame.setLayout(null);
         frame.setVisible(true);
-        
+    }
+
+    static void createFrameNewEvents(){
+        JFrame frame=new JFrame();  
+        frame.setSize(1000,1000);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    static void createFrameEdit(){
+        JFrame frame=new JFrame();  
+        frame.setSize(1000,1000);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    static void createFrameCurrentEvents(){
+        JFrame frame=new JFrame();  
+        frame.setSize(1000,1000);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
