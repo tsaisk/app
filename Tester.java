1d81b79d01c25cbfc9cee88e33f8e2a5366be3d6
Kyle Mullins <kylebmullins@gmail.com>
2021-04-11 19:01:36 -0600

    Added commands to Tester.java so teammates know what to use

--- Tester.java
+++ Tester.java
@@ -2,11 +2,15 @@ import java.io.*;
 
 public class Tester {
     public static void main(String[] args) throws IOException, FileNotFoundException {
-        //NewRemind test = new NewRemind("Test", "This is a test", 1, 1, 22, 12, 24);
-        //EditRemind.editEvent("Test", "New test");
-        //EditRemind.editDate("hello", 7, 12, 21);
-        //EditRemind.editTime("hello", 5, 37);
-        getRemind.getEvent("Test");
-        getRemind.getMinute("hello");
+        //NewRemind test = new NewRemind("Test", "This is a test", 1, 1, 22, 12, 24);  this is the command to create a new reminder
+        //EditRemind.editEvent("Test", "New test");    this is the command to edit the reminder
+        //EditRemind.editDate("hello", 7, 12, 21);     this is the command to edit the day the reminder happens on
+        //EditRemind.editTime("hello", 5, 37);     this is the command to edit the time of day the reminder happens on
+        //String string = getRemind.getEvent("Test");    this is the command to set a string to the event on the file
+        //int day = getRemind.getDay("Test");    this is the command to set an integer to the day on the file
+        //int month = getRemind.getMonth("Test");    this is the command to set an integer to the month on the file
+        //int year = getRemind.getYear("Test");    this is the command to set an integer to the year on the file
+        //int hour = getRemind.getHour("Test");    this is the command to set an integer to the hour on the file
+        //int minute = getRemind.getMinute("hello");    this is the command to set an integer to the minute on the file
     }
 }
