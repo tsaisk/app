bc1372e6bffa54e6ce7cc015068e5bf67e284846
Kyle Mullins <kylebmullins@gmail.com>
2021-04-11 19:35:01 -0600

    fixed NewRemind.java

--- NewRemind.java
+++ NewRemind.java
@@ -4,24 +4,8 @@ public class NewRemind {
     String fileName = "", event = "";
     int day, month, year, hour, minute;
 
-
-public NewRemind(String remindName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
-    makeEvent(remindName, "'" + reminder + "'", day, month, year);
-}
     public NewRemind(String remindName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
         makeEvent(remindName, "'" + reminder + "'", day, month, year, hour, minute);
-    }
-
-public void makeEvent(String eventName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
-    File file = new File ("reminders.md");
-
-    String write = findEnd(file) + eventName + "," + reminder + "," + day + "/" + month + "/" + year;
-
-    PrintWriter game = new PrintWriter("reminders.md");
-
-    game.write(write);
-    game.flush();
-    game.close();
     }
 
     public void makeEvent(String eventName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
