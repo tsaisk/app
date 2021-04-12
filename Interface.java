7d90559c7822d36dfb95401b99f22bb52508b2d1
Chandan Marle <chandanmarlex12@gmail.com>
2021-04-11 20:47:12 -0600

    test 3

--- Interface.java
+++ Interface.java
@@ -1,4 +1,5 @@
 import javax.swing.*;  
+import java.io.*;
 public class Interface {
     
     public static void main(String[] args){
@@ -43,11 +44,43 @@ public class Interface {
         frame.setVisible(true);
     }
 
-    static void createFrameNewEvents(){
+    static void createFrameNewEvents() throws IOException, FileNotFoundException{
         JFrame frame=new JFrame();  
+        JTextField year, month, day, hour, minute;
+        JButton add;
+
         frame.setSize(1000,1000);
         frame.setLayout(null);
         frame.setVisible(true);
+        
+        year = new JTextField("Year");
+        year.setBounds(100,400,150,20); 
+        
+        month = new JTextField("Month");
+        month.setBounds(250,400,150,20); 
+        
+        day = new JTextField("Day");
+        day.setBounds(400,400,150,20); 
+        
+        hour = new JTextField("Hour");
+        hour.setBounds(550,400,150,20); 
+        
+        minute = new JTextField("Minute");
+        minute.setBounds(700,400,150,20); 
+        
+        add = new JButton();
+        add.setBounds(850,400,40,20); 
+        add.addActionListener((e)->  {
+            NewRemind test = new NewRemind("Test", "This is a test", 1, 1, 22, 12, 24);
+        });
+
+        frame.add(year);
+        frame.add(month);
+        frame.add(day);
+        frame.add(hour);
+        frame.add(minute);
+        frame.add(add);
+        
     }
 
     static void createFrameEdit(){
