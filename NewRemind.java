01c5f88eb4096b04eb93ff4868e73390636f3e31
Chandan Marle <chandanmarlex12@gmail.com>
2021-04-11 19:33:21 -0600

    test 2

--- NewRemind.java
+++ NewRemind.java
@@ -5,12 +5,10 @@ public class NewRemind {
     int day, month, year, hour, minute;
 
 
-public NewRemind(String remindName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
-    makeEvent(remindName, "'" + reminder + "'", day, month, year);
+
+public NewRemind(String remindName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
+    makeEvent(remindName, "'" + reminder + "'", day, month, year, hour, minute);
 }
-    public NewRemind(String remindName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
-        makeEvent(remindName, "'" + reminder + "'", day, month, year, hour, minute);
-    }
 
 public void makeEvent(String eventName, String reminder, int day, int month, int year) throws IOException, FileNotFoundException {
     File file = new File ("reminders.md");
@@ -24,17 +22,6 @@ public void makeEvent(String eventName, String reminder, int day, int month, int
     game.close();
     }
 
-    public void makeEvent(String eventName, String reminder, int day, int month, int year, int hour, int minute) throws IOException, FileNotFoundException {
-        File file = new File ("reminders.md");
-    
-        String write = findEnd(file) + eventName + "," + reminder + "," + day + "/" + month + "/" + year + "," + hour + ":" + minute;
-    
-        PrintWriter game = new PrintWriter("reminders.md");
-    
-        game.write(write);
-        game.flush();
-        game.close();
-        }
 
     public String findEnd(File fileName) throws IOException, FileNotFoundException {
         Scanner scan = new Scanner(fileName);
